4cab01c7e0347235efbd5d4a1e773b5115323f97
Peng YU <peng.yu@shopify.com>
2019-06-14 14:43:02 -0400

    I have made this Change

--- Desktop/AI/Bean/query_score.py
+++ Desktop/AI/Bean/query_score.py
@@ -1,12 +1,12 @@
 ## QUERY X DESC FUNCTION ##
-import keyword_1 
+import keyword_1
+
 
 def query_score(user_query, service_description, SID):
 
 	query = keyword_extract(user_query)
 	desc = keyword_extract(service_description)
 
-	score = 
+	score = xxx
 
 	return score
-
